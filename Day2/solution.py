ef08bb36d97464d2474d4d22888de536b574eb92
Joe McConnell <joe.mcconnell10@gmail.com>
2025-12-06 22:11:01 -0600

    Completed 2b

--- Day2/solution.py
+++ Day2/solution.py
@@ -6,24 +6,34 @@ import utils
 
 BASE_DIR = Path(__file__).parent  # Day1 folder
 
-def findInvalid(strRange):
+def findInvalid(strRange, invalidID_list):
     start = int(strRange.split("-")[0])
     stop = int(strRange.split("-")[1])
-
-    inValidID = []
 
     for ID in range(start, stop +1):
         strID = str(ID)
         charCount = len(strID)
         halfChar = int(charCount/2)
-        if charCount % 2 == 0:
-            firstHalf = strID[:halfChar]
-            secondHalf = strID[-halfChar:]
-            if firstHalf == secondHalf:
-                print(f'Invalid ID: {strID}')
-                inValidID.append(ID)
 
-    return inValidID
+        for i in range(1, halfChar + 1):
+            invalid = True
+            if charCount % i == 0:
+                c = list(utils.chunks(strID, i))
+
+                first = c[0]
+
+                for j in range(1, len(c)):
+                    if first != c[j]:
+                        invalid = False
+                        break
+            else:
+                invalid = False
+            if invalid == True and ID not in invalidID_list:
+                invalidID_list.append(ID)
+                print(f'Invalid ID: {ID}')
+
+    return invalidID_list
+
 
 if __name__ == "__main__":
     # Allow overriding file name from command line
@@ -34,10 +44,12 @@ if __name__ == "__main__":
     rangeList = data.split(",")
 
     sum = 0
+    invalidID_list = []
     for strRange in rangeList:
-        invalidIDs = findInvalid(strRange)
+        invalidID_list = findInvalid(strRange, invalidID_list)
 
-        for ID in invalidIDs:
-            sum += ID
+    print(invalidID_list)
+    for ID in invalidID_list:
+        sum += ID
 
     print(f'Sum: {sum}')
