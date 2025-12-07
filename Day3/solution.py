0b47be8bbda9181f2fcdda6ad15c01923ac7745f
Joe McConnell <joe.mcconnell10@gmail.com>
2025-12-07 08:25:18 -0600

    Completed 3a

--- Day3/solution.py
+++ Day3/solution.py
@@ -6,16 +6,41 @@ import utils
 
 BASE_DIR = Path(__file__).parent  # Day1 folder
 
-def part1(data):
-    return None
+def getResult(line):
+    nums = []
+    for ch in line:
+        nums.append(int(ch))
 
-def part2(data):
-    return None
+    highFirst = nums[0]
+    firstPosition = 0
+    for i in range(1, len(nums) - 1):
+        if highFirst < nums[i]:
+            highFirst = nums[i]
+            firstPosition = i
+
+    highSecond = nums[firstPosition +1]
+    secondPostion = firstPosition +1
+
+    for i in range(secondPostion, len(nums)):
+        if highSecond < nums[i]:
+            highSecond = nums[i]
+            secondPostion = i
+
+    highValue = int(str(highFirst) + str(highSecond))
+
+    return highValue
 
 if __name__ == "__main__":
     # Allow overriding file name from command line
     filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
-    data = utils.read_text(BASE_DIR / filename)
+    data = utils.read_lines(BASE_DIR / filename)
 
-    print(data)
+    total = 0
+
+    for line in data:
+        result = getResult(line)
+        total += result
+        print(f'Result: {result}')
+
+    print(f'Total: {total}')
