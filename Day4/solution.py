f7b20d3074fee5088bd24380849b7cb9b2bfe18f
Joe McConnell <joe.mcconnell10@gmail.com>
2025-12-08 13:21:54 -0600

    Completed 4a and 4b

--- Day4/solution.py
+++ Day4/solution.py
@@ -6,8 +6,24 @@ import utils
 
 BASE_DIR = Path(__file__).parent  # Day1 folder
 
-def part1(data):
-    return None
+def searchGrid(data):
+    availableTP = []
+    for pos, ch in gridDict.items():
+        counter = 0
+        if ch == "@":
+            neighbors = utils.neighbors8(pos)
+            for npos in utils.neighbors8(pos):
+                if npos not in gridDict:
+                    continue
+                nch = gridDict[npos]
+                if nch == "@":
+                    counter += 1
+            if counter < 4:
+                availableTP.append((pos, ch))
+                gridDict[pos] = "."
+
+
+    return availableTP
 
 def part2(data):
     return None
@@ -16,6 +32,17 @@ if __name__ == "__main__":
     # Allow overriding file name from command line
     filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
-    data = utils.read_text(BASE_DIR / filename)
+    total = 0
+    data = utils.read_lines(BASE_DIR / filename)
+    gridDict = utils.grid_from_lines(data)
 
-    print(data)
+    innerCounter = True
+
+    while innerCounter == True :
+        availableTP = searchGrid(gridDict)
+        total += len(availableTP)
+        if len(availableTP) == 0:
+            innerCounter = False
+
+    print(total)
+
