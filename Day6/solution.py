28b06abea1166a53a81070cdef09cfe15e162ade
Joe McConnell <joe.mcconnell10@gmail.com>
2025-12-13 12:16:42 -0600

    finished 6a

--- Day6/solution.py
+++ Day6/solution.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from idlelib.outwin import file_line_pats
 from pathlib import Path
 import sys
 
@@ -6,16 +7,54 @@ import utils
 
 BASE_DIR = Path(__file__).parent  # Day1 folder
 
-def part1(data):
-    return None
+def createMatrix(data):
+    columns = len(data[0].split())
+    rows = len(data)
+    matrix = []
+    flipMatrix = []
 
-def part2(data):
-    return None
+
+    for line in data:
+        matrix.append(line.split())
+
+    for j in range(columns):
+        list = []
+        for k in range(rows):
+            list.append(matrix[k][j])
+        flipMatrix.append(list)
+
+    return flipMatrix
+
+def doMath(matrix):
+    i=0
+    results = []
+    for line in matrix:
+        operator = line[len(line) - 1]
+        if operator == "*":
+            result = 1
+            for number in line:
+                if number !="*":
+                    result *= int(number)
+            results.append(result)
+        if operator == "+":
+            result = 0
+            for number in line:
+                if number != "+":
+                    result += int(number)
+            results.append(result)
+    return results
 
 if __name__ == "__main__":
     # Allow overriding file name from command line
     filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
-    data = utils.read_text(BASE_DIR / filename)
+    data = utils.read_lines(BASE_DIR / filename)
 
-    print(data)
+    fullArray = createMatrix(data)
+
+    resultsList = doMath(fullArray)
+
+    sum = 0
+    for result in  resultsList:
+        sum += result
+    print(sum)
